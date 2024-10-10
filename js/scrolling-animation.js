9e3d7107a9b2197b357a2f9cddc51de76eb9b355
Alexander Bermudez <73725686+Alex-Bermudez@users.noreply.github.com>
2024-10-10 15:29:27 -0700

    Updated Hero

--- js/scrolling-animation.js
+++ js/scrolling-animation.js
@@ -20,3 +20,28 @@ const sections = document.querySelectorAll('section');
 sections.forEach(section => {
     observer.observe(section);
 });
+
+// JavaScript to handle fade-up on smaller screens for hero
+document.addEventListener("DOMContentLoaded", function () {
+    const lines = document.querySelectorAll(".line");
+
+    const observer = new IntersectionObserver(
+        (entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("fade-up");
+                } else {
+                    entry.target.classList.remove("fade-up");
+                }
+            });
+        },
+        {
+            threshold: 0.1, // Trigger when 10% of the element is visible
+        }
+    );
+
+    // Apply observer only if screen width is less than or equal to 768px
+    if (window.innerWidth <= 768) {
+        lines.forEach(line => observer.observe(line));
+    }
+});
