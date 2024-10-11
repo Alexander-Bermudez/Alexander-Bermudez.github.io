21c6a9b3e82e51b7b2fb08fdae516216710d59bd
Alexander Bermudez <alex.bermudez0222@gmail.com>
2024-10-11 14:34:35 -0700

    Added hamburger bar animation

--- js/script.js
+++ js/script.js
@@ -4,6 +4,14 @@ const navLinks = document.getElementById('nav-links');
 // Toggle the 'show' class to control visibility when the hamburger is clicked
 hamburger.onclick = function() {
     navLinks.classList.toggle('show'); // Toggle 'show' class to control visibility
+
+    if (navLinks.classList.contains('show')) {
+        // Calculate the height dynamically and apply it
+        const linksHeight = navLinks.scrollHeight + 'px'; // Get the height of the content
+        navLinks.style.height = linksHeight; // Set the height to the content height
+    } else {
+        navLinks.style.height = '0'; // Collapse back to height 0
+    }
 };
 
 // Close the menu when a link is clicked
@@ -12,5 +20,6 @@ const links = navLinks.getElementsByTagName('a'); // Get all anchor links in the
 for (let i = 0; i < links.length; i++) {
     links[i].onclick = function() {
         navLinks.classList.remove('show'); // Remove 'show' class to hide the menu
+        navLinks.style.height = '0'; // Collapse back to height 0
     };
 }
